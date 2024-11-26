645bce71c19a4c06b2a82c4f774a58732f5608c7
jonathansan04 <tatansantos4@gmail.com>
2024-11-25 20:48:20 -0500

    Arreglo CORS

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000; // Puerto de la API
 const app = express();
 
 // Middlewares
-app.use(cors()); 
+app.use(cors({ origin: 'http://localhost:5173',   credentials: true}));
 app.use(morgan('dev')); 
 app.use(express.json()); 
 app.use(cookieParser()); 
